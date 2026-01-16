0679649f979243c4ba234b8cadd7be102b8f7cd0
TrevinoRizkySunarja <you@example.com>
2026-01-16 17:13:13 +0100

    Final

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,20 @@
 import { defineConfig } from "vite";
-import { resolve, dirname } from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { resolve } from "path";
 
 export default defineConfig({
-    root: "public",
-    base: "./",
+    base: "/VormvrijeSignatuur/",
     build: {
-        outDir: "../dist",
-        emptyOutDir: true,
         rollupOptions: {
             input: {
-                index: resolve(__dirname, "docs/index.html"),
-                intro: resolve(__dirname, "docs/intro.html"),
-                hub: resolve(__dirname, "docs/hub.html"),
-                notes: resolve(__dirname, "docs/notes.html"),
-                deadlines: resolve(__dirname, "docs/deadlines.html"),
-                research: resolve(__dirname, "docs/research.html"),
-                testing: resolve(__dirname, "docs/testing.html"),
-                feedback: resolve(__dirname, "docs/feedback.html"),
-                results: resolve(__dirname, "docs/results.html"),
+                index: resolve(__dirname, "index.html"),
+                intro: resolve(__dirname, "intro.html"),
+                hub: resolve(__dirname, "hub.html"),
+                notes: resolve(__dirname, "notes.html"),
+                deadlines: resolve(__dirname, "deadlines.html"),
+                research: resolve(__dirname, "research.html"),
+                testing: resolve(__dirname, "testing.html"),
+                feedback: resolve(__dirname, "feedback.html"),
+                results: resolve(__dirname, "results.html"),
             },
         },
     },
